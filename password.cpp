84fd12e21943bea49e67092f5071648064599075
Lak5hay <tlakshay98@gmail.com>
2024-10-24 02:06:55 +0530

    Create password.cpp

---
+++ password.cpp
@@ -0,0 +1,25 @@
+//laksahy tanwar 23070123079
+#include<iostream>
+#include<string>
+
+using namespace std;
+int main()
+{
+    int password, cor_password=1234;
+    cout<<"Enter password: "<<endl;
+    cin>>password;
+    while(password>0)
+    if(password==cor_password)
+    {
+        cout<<"welcome";
+        break;
+    
+    }
+}
+/*
+Enter password: 
+1234
+welcome
+
+=== Code Execution Successful ===
+*/
