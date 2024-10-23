cbaa9c13744a0f7409b71de96e660bfad4ce0ebe
Lak5hay <tlakshay98@gmail.com>
2024-10-24 02:09:15 +0530

    Create printeverythingexceptSIT.cpp

---
+++ printeverythingexceptSIT.cpp
@@ -0,0 +1,22 @@
+// lakshay tanwar 23070123079
+#include <iostream>
+using namespace std;
+
+int main() {
+    string k;
+    cin>>k;
+    do{
+        cout<<k<<"\n";
+        cin>>k;
+    }
+    while(k!="sit");
+}
+/*
+hey
+hey
+
+sit
+
+
+=== Code Execution Successful ===
+*/
