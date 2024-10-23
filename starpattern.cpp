9c5cca53fd10e82974eb1a07464f9ae0e88a3cf5
Lak5hay <tlakshay98@gmail.com>
2024-10-24 02:10:04 +0530

    Create starpattern.cpp

---
+++ starpattern.cpp
@@ -0,0 +1,21 @@
+/*lakshay tanwar 23070123079*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, j;
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+/*
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+*/
