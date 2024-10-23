f3b8a7ee03e68dd9d411b6369f415183c71671b8
Lak5hay <tlakshay98@gmail.com>
2024-10-24 02:08:33 +0530

    Create printSIT.cpp

---
+++ printSIT.cpp
@@ -0,0 +1,16 @@
+/*lakshay tanwar 23070123079*/
+#include <iostream>
+using namespace std;
+int main() {
+    for (int i = 0; i < 5; ++i) {
+        cout << "SIT" << endl;
+    }
+    return 0;
+}
+/*
+SIT
+SIT
+SIT
+SIT
+SIT
+*/
